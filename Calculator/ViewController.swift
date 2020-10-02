21256a34b58108aacc29121da269c8cbe71ba134
Supriya G <sgadkar1@my.centennialcollege.ca>
2020-10-01 22:35:32 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Supriya Gadkari on 09/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}

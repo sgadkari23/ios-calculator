b2dadc0b3646f47167f73cfffbd57dd298648edb
Supriya G <sgadkar1@my.centennialcollege.ca>
2020-10-02 18:46:05 -0500

    Metadata in ViewController file

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,9 +1,12 @@
 //
 //  ViewController.swift
 //  Calculator
-//
+//  App Name: Arithmetic Calculator
 //  Created by Supriya Gadkari on 09/27/20.
-//
+//  Student ID: 301140872
+//  App Description: A Simple arithmetic calculator built using storyboard UI and swift
+//  App version: 0.1
+//  XCode version: 12.0.1
 
 import UIKit
 
@@ -11,6 +14,5 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }

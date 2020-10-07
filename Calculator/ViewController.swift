d9aea380b8ec743b487ed1c5e0cba56cfaa108d4
Supriya G <sgadkar1@my.centennialcollege.ca>
2020-10-06 21:41:26 -0500

    Basic arithmetic operation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,8 +11,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func onNumberButton_Pressed(_ sender: UIButton) {
+        
+        print(sender.titleLabel!.text!);
+        if(resultLabel.text! == "0") {
+            resultLabel.text! = sender.titleLabel!.text!
+        }
+        else {
+            resultLabel.text! += sender.titleLabel!.text!
+        }
+    }
+    
+
+    @IBAction func onArithmethicButton_Pressed(_ sender: UIButton) {
+        //print(sender.titleLabel!.text!);
+        
+        switch sender.titleLabel!.text! {
+        case "C":
+            resultLabel.text! = "0"
+        case "+":
+            resultLabel.text! = "+"
+        default:
+            resultLabel.text! += sender.titleLabel!.text!
+        }
+        
+        
+        
+    }
+    
 }
